0de609fe9693123e7ecebdea30cf966bab9df879
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-23 21:04:02 +0100

    Add name checking function and when conditions met call new pokedata

--- wtpokemon/public/scripts/indexMain.js
+++ wtpokemon/public/scripts/indexMain.js
@@ -2,16 +2,26 @@
 const vApp = new Vue({
     el: '#vApp',
     data: {
-        randNum : 1
+        randNum : 1,
+        userInput: ''
     },
     methods: {
         startGame: function() {
             this.randNum = Math.ceil(Math.random() * 150)
+        },
+        checkName: function() {
+            if(this.userInput.toLowerCase() == this.pokeName) {
+                this.userInput = '';
+                this.startGame();
+            }
         }
     },
     computed: {
         imgBack : function() {
             return `./images/pokemon/${this.randNum}.png`;
+        },
+        pokeName: function() {
+            return pokeData[this.randNum - 1].name;
         }
     }
 })
