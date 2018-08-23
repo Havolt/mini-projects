aeb7b42f9dafa7d468e539b0d6ab8e9fbe5aff6a
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-23 19:39:20 +0100

    Add pokemon data

--- wtpokemon/routes/index.js
+++ wtpokemon/routes/index.js
@@ -9,24 +9,8 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  let pokeType = 'not been changed';
-  const newPoke = {}
   
-    request(`https://pokeapi.co/api/v2/pokemon/103/`, (err, res, body) =>{
-      if(err) {
-        console.log(err);
-      } else {
-        let rtrnPoke = JSON.parse(body);
-        newPoke.name = rtrnPoke.name;
-        newPoke.id = rtrnPoke.id;
-        fs.appendFileSync('pokeData.txt', JSON.stringify(newPoke) + ', \n');
-      }
-    });
-    //setTimeout(() => fs.appendFileSync('pokeData.txt', JSON.stringify(newPoke) + ', \n'), 2000);
-
-
-  
-  //res.sendFile(path.join(__dirname, './../views/index.html'));
+  res.sendFile(path.join(__dirname, './../views/index.html'));
 });
 
 module.exports = router;
