53e9a140a84c2f764de2ef89907686c04a41a926
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-24 14:13:46 +0100

    Add num and toggle hidden when win scenario is active

--- wtpokemon/public/scripts/indexMain.js
+++ wtpokemon/public/scripts/indexMain.js
@@ -7,16 +7,24 @@ const vApp = new Vue({
         pokeImgClassObject: {
             'poke-img-hidden': true,
             '.poke-img-reveal': false
+        },
+        pokeNumClassObject: {
+            'poke-num-hidden': true
         }
     },
     methods: {
         startGame: function() {
+            this.userInput = '';
+            this.pokeImgClassObject['poke-img-hidden'] = true;
+            this.pokeImgClassObject['poke-img-reveal'] = false;
+            this.pokeNumClassObject['poke-num-hidden'] = true;
             this.randNum = Math.ceil(Math.random() * 150)
         },
         checkName: function() {
             if(this.userInput.toLowerCase() == this.pokeName) {
-                this.userInput = '';
-                this.startGame();
+                this.pokeImgClassObject['poke-img-hidden'] = false;
+                this.pokeImgClassObject['poke-img-reveal'] = true;
+                this.pokeNumClassObject['poke-num-hidden'] = false;
             }
         }
     },
@@ -26,6 +34,17 @@ const vApp = new Vue({
         },
         pokeName: function() {
             return pokeData[this.randNum - 1].name;
+        },
+        pokeNum: function () {
+            if(pokeData[this.randNum - 1].id < 10) {
+                return `#00${pokeData[this.randNum - 1].id}`;
+            }
+            else if(pokeData[this.randNum - 1].id < 100) {
+                return `#0${pokeData[this.randNum - 1].id}`;
+            }
+            else {
+                return `#${pokeData[this.randNum - 1].id}`;
+            }
         },
         pokeNamePartial: function () {
             let inpSplit = this.userInput.split('');
@@ -40,7 +59,7 @@ const vApp = new Vue({
                     return totalName.join('');
                 }
             }
-
+           
             return totalName.join('');
         }
     }
