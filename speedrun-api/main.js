cba2959d6d3447b1b92f4e316901e886d8805824
Mark Fitzpatrick <markfitz815@gmail.com>
2018-09-03 20:24:50 +0100

    add graph with sizing to display difference in best runs

--- speedrun-api/main.js
+++ speedrun-api/main.js
@@ -1,5 +1,3 @@
-console.log('hello');
-
 const runData = {runInOrder: [], runInOrderBest: []};
 
 fetch('https://www.speedrun.com/api/v1/leaderboards/pd0wq31e/category/7kjrn323')
@@ -47,13 +45,53 @@ function sortRunsDate(rd) {
     }
 }
 
-function sortRunsTime(rd) {
+function sortRunsTime(rd, best) {
+    best.push(rd[0]);
+    for(let i = 1; i < rd.length; i++) {
+        if(rd[i].place < best[best.length-1].place) {
+            best.push(rd[i]);
+        }
+    }
+}
+
+function drawChart(rd) {
+    const app = document.querySelector('.app');
+    const colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'yellow']
+    let sizes = [];
+    let topSize;
+    let perSizes = [];
+
     for(let i = 0; i < rd.length; i++) {
-        
+        sizes.push((rd[i].run.times.primary_t - rd[rd.length-1].run.times.primary_t) + 50);
+    }
+
+    topSize = sizes[0] / 100;
+
+    for(let i = 0; i < sizes.length; i++) {
+        perSizes.push(sizes[i] / topSize);
+    }
+    
+    for(let i = 0; i < perSizes.length; i++) {
+        const container = document.createElement('div');
+        container.style.height="100%";
+        container.style.display="inline-block"
+        const el = document.createElement('div');
+        el.style.background=colors[(i+7)%7];
+        el.style.width="50px";
+        el.style.height=perSizes[i]+'%';
+        el.style.display="inline-block"
+        const time = document.createElement('div');
+        time.style.fontWeight="bold";
+        time.style.textAlign="center";
+        time.innerHTML = Math.round(rd[i].run.times.primary_t / 60);
+        container.appendChild(el);
+        container.appendChild(time);
+        app.appendChild(container);
     }
 }
 
 function runApp() {
     sortRunsDate(runData);
-    sortRunsTime(runData.runInOrder);
+    sortRunsTime(runData.runInOrder, runData.runInOrderBest);
+    drawChart(runData.runInOrderBest);
 }
