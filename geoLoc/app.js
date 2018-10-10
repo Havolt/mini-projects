5c867ebcc5ee329b77f5d6652f431eeb7780645a
Mark Fitzpatrick <markfitz815@gmail.com>
2018-10-10 15:20:22 +0100

    add backend to geoLoc

---
+++ geoLoc/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
