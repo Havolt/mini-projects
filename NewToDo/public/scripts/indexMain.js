106a62052f13f39f37ba4ffac151bab6a1333958
Mark Fitzpatrick <markfitz815@gmail.com>
2018-12-04 18:45:47 +0000

    button now adds input data to list

--- NewToDo/public/scripts/indexMain.js
+++ NewToDo/public/scripts/indexMain.js
@@ -1,15 +1,42 @@
 
-function getData() {
+const todoList = new Array();
 
+function manipulateListHTML(arr) {
+    console.log(arr);
 }
 
-function checkInput() {
-    console.log('here');
+function clearInput() {
+    document.querySelector('.inputEvent').value = '';
+    document.querySelector('.inputDeadline').value = '';
+    document.querySelector('.inputLocation').value = '';
+}
+
+function checkInput(todo, deadline, location) {
+    
+    if(todo) {
+        const newDeadline = new Date(deadline);
+        const newObj = {};
+        newObj.todo = todo;
+        if(!isNaN(newDeadline.getDate()) ) {
+            newObj.deadline = newDeadline;
+        }
+        if(location) {
+            newObj.location = location;
+        }
+        todoList.push(newObj);
+        todo = ''
+        manipulateListHTML(todoList);
+        clearInput();
+    }
 }
 
 function evtListeners() {
     document.querySelector('.inputButton').addEventListener('click', () => {
-        checkInput();
+        checkInput(
+            document.querySelector('.inputEvent').value,
+            document.querySelector('.inputDeadline').value,
+            document.querySelector('.inputLocation').value
+            );
     });
 };
 
