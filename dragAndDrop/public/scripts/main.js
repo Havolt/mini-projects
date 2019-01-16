cb1018c17758d8691887e781a166e52151f387c4
Mark Fitzpatrick <markfitz815@gmail.com>
2019-01-16 19:33:37 +0000

    add basic movement

--- dragAndDrop/public/scripts/main.js
+++ dragAndDrop/public/scripts/main.js
@@ -5,32 +5,47 @@ let dragData = {
     userDragBool: false,
     userBeginX: undefined,
     userBeginY: undefined,
-    userInitMouseX: undefined,
-    userInitMouseY: undefined
+    userDisplaceX: 0,
+    userDisplaceY: 0,
+    userInitMouseX: 0,
+    userInitMouseY: 0
 }
 
 function itemHeld(val, e) {
-    dragData.userDragBool = true;
-    dragData.userDiv = val;
-    dragData.userBeginX = val.getBoundingClientRect().left;
-    dragData.userBeginY = val.getBoundingClientRect().top;
-    val.classList.add('posAb')
-    dragData.userInitMouseX = e.clientX;
-    dragData.userInitMouseY = e.clientY;
+        dragData.userDragBool = true;
+        dragData.userDiv = val;
+        dragData.userBeginX = val.getBoundingClientRect().left;
+        dragData.userBeginY = val.getBoundingClientRect().top;
+        val.classList.add('posAb')
+        
+        dragData.initMouseX = e.clientX;
+        dragData.initMouseY = e.clientY;
+
+        console.log(dragData.userInitMouseX, dragData.userInitMouseY);
+    
 }
 
 function moveItem(ob, ev) {
+    
     if(ob.userDragBool) {
-        console.log(ob.userInitMouseX)
+        console.log(ev.client)
+        //ob.userDiv.style.transform = `translateX(${ob.userInitMouseX + ev.clientX})`
+        //ob.userDiv.style.transform = `translateY(${ob.userInitMouseY + ev.clientY})`
+        ob.userDiv.style.top = `${ob.userInitMouseY + ev.clientY}px`;
+        ob.userDiv.style.left = `${ob.userInitMouseX + ev.clientX}px`;
+        
+        
     }
 }
 
 function itemLetGo() {
-    dragData.userDiv.classList.remove('posAb')
-    dragData.userDragBool = false;
-    dragData.userDiv = undefined;
-    dragData.userBeginX = undefined;
-    dragData.userBeginY = undefined;
+    if(dragData.userDragBool) {
+        dragData.userDiv.classList.remove('posAb')
+        dragData.userDragBool = false;
+        dragData.userDiv = undefined;
+        dragData.userBeginX = undefined;
+        dragData.userBeginY = undefined;
+    }
     
 }
 
