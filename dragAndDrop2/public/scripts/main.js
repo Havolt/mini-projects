9dd40b1d327bb39d5f7bc6dcd8154cf311247b37
Mark Fitzpatrick <markfitz815@gmail.com>
2019-01-22 17:53:54 +0000

    add comments and create array in html function of elements in correct order

--- dragAndDrop2/public/scripts/main.js
+++ dragAndDrop2/public/scripts/main.js
@@ -2,21 +2,33 @@
 const listData = {
     initWords: ['Squall', 'Quistis', 'Seifer', 'Zell', 'Rinoa', 'Irvine'],
     list: [],
+    //Puts elements in list
     putWordInList: function(arr) {
-        arr.map((el)=>{
+        arr.map((el, ind)=>{
             const newOb = {};
             newOb.name = el;
-            newOb.pos = arr.length+1;
+            newOb.pos = listData.list.length;
             newOb.moved = false;
             newOb.movedDir = 0;
             listData.list.push(newOb);
         })
     },
+    //Creates HTML on index page of list
     createHMTL: function(arr) {
-        console.log(arr)
+        document.querySelector('.list-contain').innerHTML = '';
+
+        //Create an array with all elements positioned by el.pos
+        const newArr = [];
+        arr.map((el, ind) => {
+            newArr[el.pos] = el;
+        })
+
+        console.log(newArr);
+
     }
 };
 
+//Runs on page load
 (function initApp() {
     listData.putWordInList(listData.initWords);
     listData.createHMTL(listData.list);
