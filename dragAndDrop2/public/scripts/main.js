b96093b98234e70bfd6624a62ff1720d98e4d8d8
Mark Fitzpatrick <markfitz815@gmail.com>
2019-01-22 16:37:16 +0000

    add createHTML method and give list further properties

--- dragAndDrop2/public/scripts/main.js
+++ dragAndDrop2/public/scripts/main.js
@@ -4,12 +4,22 @@ const listData = {
     list: [],
     putWordInList: function(arr) {
         arr.map((el)=>{
-            listData.list.push(el);
+            const newOb = {};
+            newOb.name = el;
+            newOb.pos = arr.length+1;
+            newOb.moved = false;
+            newOb.movedDir = 0;
+            listData.list.push(newOb);
         })
+    },
+    createHMTL: function(arr) {
+        console.log(arr)
     }
 };
 
 (function initApp() {
     listData.putWordInList(listData.initWords);
-    console.log(listData.list)
+    listData.createHMTL(listData.list);
+
+    
 })()
