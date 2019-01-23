380646432c06586951400d871f966786e8a25999
Mark Fitzpatrick <markfitz815@gmail.com>
2019-01-23 19:10:55 +0000

    all list items now fix into correct place when drag is completed

--- dragAndDrop2/public/scripts/main.js
+++ dragAndDrop2/public/scripts/main.js
@@ -1,5 +1,6 @@
 
 const listData = {
+    dragDistance: 60,
     containDivData : document.querySelector('.list-contain').getBoundingClientRect(),
     itemHeld: false,
     selectedItem: '',
@@ -20,7 +21,7 @@ const listData = {
         })
     },
     //Creates HTML on index page of list
-    createHMTL: function(arr) {
+    createHTML: function(arr) {
         document.querySelector('.list-contain').innerHTML = '';
 
         //Create an array with all elements positioned by el.pos
@@ -47,7 +48,7 @@ const listData = {
             const newArr = [];
             newArr.push(item.value);
             this.putWordInList(newArr);
-            this.createHMTL(this.list);
+            this.createHTML(this.list);
             item.value = '';
         }
     },
@@ -74,11 +75,38 @@ const listData = {
     },
     letGoList: function() {
         if(listData.itemHeld) {
+            //Change styling
             console.log('got here')
             listData.selectedElement.style.zIndex = 'auto';
             listData.dragLi(0, 0)
             listData.selectedElement.classList.remove('dad-li-inner-grab');
             listData.selectedElement.style.setProperty("transform", 'none');
+
+            //Change positions
+            if((listData.selectedItem.selected.dragY / listData.dragDistance) >= 1) {
+                //console.log(Math.floor(listData.selectedItem.selected.dragY / listData.dragDistance))
+                for(let i = 0; i < listData.list.length; i++) {
+                    if(listData.list[i].movedDir == -1) {
+                        console.log('yush')
+                        listData.list[i].pos--;
+                    }
+                }
+                listData.selectedItem.pos += Math.floor(listData.selectedItem.selected.dragY/listData.dragDistance)
+                listData.createHTML(listData.list);
+            }
+            else if((listData.selectedItem.selected.dragY / listData.dragDistance) <= -1) {
+                for(let i = 0; i < listData.list.length; i++) {
+                    if(listData.list[i].movedDir == 1) {
+                        console.log('yush')
+                        listData.list[i].pos++;
+                    }
+                }
+                listData.selectedItem.pos += Math.ceil(listData.selectedItem.selected.dragY/listData.dragDistance)
+                //console.log(Math.ceil(listData.selectedItem.selected.dragY / listData.dragDistance))
+                listData.createHTML(listData.list);
+            }
+
+            //Revert all changes
             listData.itemHeld = false;
             listData.selectedItem.selected.bool = false;
             listData.selectedItem.selected.dragX = 0;
@@ -86,7 +114,7 @@ const listData = {
             listData.selectedItem.selected.initX = 0;
             listData.selectedItem.selected.initY = 0;
             listData.selectedElement = '';
-            console.log(listData.selectedItem);
+
             listData.selectedItem = '';
             listData.list.map((el) => {
                 el.movedDir = 0;
@@ -109,7 +137,7 @@ const listData = {
         if(y > 10) {
             listData.list.map((el) => {
                 if(el.pos > (listData.selectedItem.pos)) {
-                    if(el.pos < listData.selectedItem.pos + (y / 60)) {
+                    if(el.pos < listData.selectedItem.pos + (y / listData.dragDistance)) {
                         console.log(el)
                         el.movedDir = -1;
                     } else {
@@ -123,8 +151,7 @@ const listData = {
         else if(y < -10) {
             listData.list.map((el) => {
                 if(el.pos < (listData.selectedItem.pos)) {
-                    if(el.pos > listData.selectedItem.pos + (-y / -60)) {
-                      
+                    if(el.pos > listData.selectedItem.pos + (-y / -listData.dragDistance)) {
                         el.movedDir = 1;
                     } else {
                         el.movedDir = 0;
@@ -157,5 +184,5 @@ const listData = {
 //Runs on page load
 (function initApp() {
     listData.putWordInList(listData.initWords);
-    listData.createHMTL(listData.list);
+    listData.createHTML(listData.list);
 })()
