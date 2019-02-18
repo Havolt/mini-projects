d021318d0e8d7e36fd7b513cc4bb5224d2abe8a5
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-18 17:17:53 +0000

    vue monster slayer: add move list and hide skillbar when not in use

--- Complete-Vue/Monster Slayer/scripts/main.js
+++ Complete-Vue/Monster Slayer/scripts/main.js
@@ -1,6 +1,7 @@
 const vm = new Vue({
     el: '#app',
     data: {
+        moveList: [],
         gameState: true,
         player: {
             health: 100
@@ -12,21 +13,31 @@ const vm = new Vue({
     methods: {
         attack(attacker) {
             const atkPower = Math.ceil((Math.random() * 8) + 2)
-            if(attacker == 0) { this.monster.health -= atkPower; this.enemyTurn()}
-            else{this.player.health -= atkPower}
+            if(attacker == 0) { 
+                this.monster.health -= atkPower; 
+                this.addToMoveList('player', 'monster', atkPower, 'hits');
+                this.enemyTurn();
+                
+            }
+            else{
+                this.player.health -= atkPower;
+                this.addToMoveList('monster', 'player', atkPower, 'hits');
+            }
         },
         spAttack() {
             const spAtkPower = Math.ceil((Math.random() * 25) + 5)
             const atkChance = Math.random();
             if(atkChance > 0.45) {
                 this.monster.health -= spAtkPower; 
+                this.addToMoveList('player', 'monster', spAtkPower, 'super hits');
             } else {
-                console.log('miss')
+                this.addToMoveList('player', 'monster', 0, 'misses');
             }
             this.enemyTurn();
         },
         heal() {
-            const healPower = Math.ceil((Math.random() * 15)+ 10)
+            const healPower = Math.ceil((Math.random() * 15)+ 10);
+            this.addToMoveList('player', 'himself', healPower, 'heals');
             
             this.player.health += healPower;
             if(this.player.health > 100) {
@@ -35,10 +46,39 @@ const vm = new Vue({
             this.enemyTurn();
         },
         enemyTurn() {
-            this.attack(1)
+            if(this.monster.health > 1) {
+                this.attack(1)
+                if(this.player.health < 1) {
+                    alert('Player Loses!')
+                }
+            } else {
+                alert('Player Wins!');
+            }
+            
+        },
+        addToMoveList(attacker, defender, amt, method) {
+            if(this.moveList.length > 12) {
+                this.moveList.pop();
+            }
+
+            const newOb = {};
+            newOb.move = `${attacker} ${method} ${defender} for ${amt}`.toUpperCase();
+            if(attacker == 'player'){
+                newOb.class = "player-turn"
+            }
+            else {
+                newOb.class = "monster-turn"
+            }
+            
+            this.moveList.unshift(newOb);
+
+
         },
         startGame() {
             console.log('starting game')
+            this.player.health = 100;
+            this.monster.health = 100;
+            this.gameState = true;
         },
         endGame() {
             this.gameState = false;
@@ -46,10 +86,10 @@ const vm = new Vue({
     },
     computed: {
         playerHealth() {
-            return this.player.health + '%';
+            return this.player.health > -1 ? this.player.health + '%' : '0%';
         },
         monsterHealth() {
-            return this.monster.health + '%';
+            return this.monster.health > -1 ? this.monster.health + '%' : '0%';
         }
     },
 })
