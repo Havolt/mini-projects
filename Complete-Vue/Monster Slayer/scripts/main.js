361714ee6f4027e8d693e130d06af6dd3597a7b6
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-18 16:38:31 +0000

    vue monster slayer: add attack, heal and sp attack commands

--- Complete-Vue/Monster Slayer/scripts/main.js
+++ Complete-Vue/Monster Slayer/scripts/main.js
@@ -10,28 +10,46 @@ const vm = new Vue({
         }
     },
     methods: {
-        attack() {
+        attack(attacker) {
             const atkPower = Math.ceil((Math.random() * 8) + 2)
-            console.log(atkPower);
+            if(attacker == 0) { this.monster.health -= atkPower; this.enemyTurn()}
+            else{this.player.health -= atkPower}
         },
         spAttack() {
             const spAtkPower = Math.ceil((Math.random() * 25) + 5)
             const atkChance = Math.random();
             if(atkChance > 0.45) {
-                console.log(spAtkPower);
+                this.monster.health -= spAtkPower; 
             } else {
                 console.log('miss')
             }
+            this.enemyTurn();
         },
         heal() {
-            const healPower = Math.ceil((Math.random() * 15)+ 5)
-            console.log(healPower);
+            const healPower = Math.ceil((Math.random() * 15)+ 10)
+            
+            this.player.health += healPower;
+            if(this.player.health > 100) {
+                this.player.health = 100;
+            }
+            this.enemyTurn();
+        },
+        enemyTurn() {
+            this.attack(1)
+        },
+        startGame() {
+            console.log('starting game')
         },
         endGame() {
             this.gameState = false;
         }
     },
     computed: {
-        
+        playerHealth() {
+            return this.player.health + '%';
+        },
+        monsterHealth() {
+            return this.monster.health + '%';
+        }
     },
 })
