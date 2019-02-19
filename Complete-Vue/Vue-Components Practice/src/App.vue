1ac6b1b4f0cf9bfd9e035dbffb8b2941e112b26e
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-19 14:09:14 +0000

    Vue Component Exercise: Organize vue app into components

---
+++ Complete-Vue/Vue-Components Practice/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <!-- Header -->
+        <Header></Header>
+        <hr>
+        <!-- Main -->
+        <MainBody></MainBody>
+        <hr>
+        <!-- Footer -->
+        <Footer></Footer>
+    </div>
+
+</template>
+    
+
+<script>
+
+    import Header from './Header.vue';
+    import MainBody from './MainBody.vue';
+    import Footer from './Footer.vue';
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+            Header,
+            MainBody,
+            Footer
+        }
+    }
+
+</script>
+
+
+
+<style>
+
+</style>
