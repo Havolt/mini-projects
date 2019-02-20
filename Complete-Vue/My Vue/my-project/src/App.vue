05e57777f66c6cc3359a827294e27e4f9abb2b70
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-20 15:11:06 +0000

    Vue Single Page App Header working

---
+++ Complete-Vue/My Vue/my-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+      <my-header></my-header>
+  </div>
+</template>
+
+<script>
+
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    myHeader: MyHeader
+  }
+}
+</script>
+
+<style>
+
+  * {
+    margin:0;
+    padding: 0;
+    border: 0;
+  }
+
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 62.5%;
+    }
+
+</style>
---
+++ Complete-Vue/My Vue/my-project/src/components/MyHeader.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="my-header">
+        <div class="header-logo-con">
+            <img class="header-logo" src="../assets/web-dev-logo.jpg" alt="web logo">
+        </div>
+        <p class="header-title">WebBizz</p>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style >
+
+    .my-header {
+        border-bottom: 1px solid rgb(26, 119, 96);
+        display: flex;
+        background: rgb(33, 197, 156);
+        
+    }
+
+    .header-logo-con {
+        flex: 1 1 auto;
+        display: flex;
+        align-content: center;
+    }
+
+    .header-logo {
+        width: 5em;
+        height: 5em;
+    }
+    
+    .header-title {
+        color: white;
+        font-size: 3em;
+        flex: 1 1 auto;
+        align-self: center;
+        text-align: right;
+        padding-right: 2em;
+    }
+
+    
+
+
+</style>
