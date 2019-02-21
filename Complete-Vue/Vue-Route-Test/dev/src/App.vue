9d8145dcf40e555af9123246656428c7c9fc7255
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-21 16:16:52 +0000

    vue router: style header

--- Complete-Vue/Vue-Route-Test/dev/src/App.vue
+++ Complete-Vue/Vue-Route-Test/dev/src/App.vue
@@ -30,6 +30,17 @@ export default {
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+html {
+  font-size: 62.5%;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -37,16 +48,5 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- Complete-Vue/Vue-Route-Test/dev/src/components/header/MainHeader.vue
+++ Complete-Vue/Vue-Route-Test/dev/src/components/header/MainHeader.vue
@@ -1,8 +1,8 @@
 <template>
-    <header id="mian-header">
+    <header id="main-header">
         <nav id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
             <router-link to="/hyrule">Hyrule</router-link>
         </nav>
     </header>
@@ -16,4 +16,31 @@ export default {
 
 <style scoped>
     
+    #main-header {
+        width: 100%;
+        height: 5rem;
+        border-bottom: 1px solid rgb(85, 41, 126);
+        background: rgb(118, 70, 163);
+        display: grid;
+        align-items: center;
+        
+    }
+
+    #nav {
+        justify-self: right;
+        padding-right: 2em;
+    }
+
+    #nav a {
+        font-size: 2em;
+        font-weight: bold;
+        color: #d8d8d8;
+        text-decoration: none;
+        padding-right: 0.6em;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #7cffc4;
+    }
+
 </style>
