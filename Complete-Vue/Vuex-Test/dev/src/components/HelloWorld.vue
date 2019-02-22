64905042155f04f4178791df323b596904986c47
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-22 11:28:19 +0000

    vuex: create first mapState link

---
+++ Complete-Vue/Vuex-Test/dev/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <p>{{msg}}</p>
+    <h1>{{myNum}}</h1>
+  </div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex';
+
+export default {
+
+  name: "HelloWorld",
+  props: {
+    
+  },
+  computed: {
+    ...mapState({
+      msg: 'msg',
+      myNum: 'myNum'
+    })
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
