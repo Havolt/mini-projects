f54b4bf8963f4d0f7c1dd27cbe137807e3dbf278
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-25 14:43:02 +0000

    python: learning basic variables/syntax

---
+++ learnPython/firstAttempts/helloWorld.py
@@ -0,0 +1,9 @@
+
+if 1 > 0:
+    print("Hello, World!")
+
+#This is a single line comment
+
+"""This is a multi-line comment
+existing on different lines
+This is thelast line of it"""
---
+++ learnPython/firstAttempts/variables.py
@@ -0,0 +1,32 @@
+"""
+x = 10
+y = "Mark"
+
+myLocation = "Ireland"
+myFaveColor = "Blue"
+myInfo = "I live in " + myLocation + " and my favourite color is " + myFaveColor
+
+print(x)
+print(y)
+
+print(myInfo)
+
+"""
+
+aa = int(2.99)
+ab = int("204")
+
+ba = float(10)
+bb = float("122")
+
+ca = str(2)
+cb = str(24.56)
+
+print(aa)
+print(ab)
+
+print(ba)
+print(bb)
+
+print(ca)
+print(cb)
