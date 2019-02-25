21a6cb2f19f3ebdb058257034c6918d348e2a57f
Mark Fitzpatrick <markfitz815@gmail.com>
2019-02-25 16:02:46 +0000

    python: go over lists and strings

--- learnPython/firstAttempts/variables.py
+++ learnPython/firstAttempts/variables.py
@@ -1,32 +1,72 @@
 """
-x = 10
-y = "Mark"
+    x = 10
+    y = "Mark"
 
-myLocation = "Ireland"
-myFaveColor = "Blue"
-myInfo = "I live in " + myLocation + " and my favourite color is " + myFaveColor
+    myLocation = "Ireland"
+    myFaveColor = "Blue"
+    myInfo = "I live in " + myLocation + " and my favourite color is " + myFaveColor
 
-print(x)
-print(y)
+    print(x)
+    print(y)
 
-print(myInfo)
+    print(myInfo)
+
+    aa = int(2.99)
+    ab = int("204")
+
+    ba = float(10)
+    bb = float("122")
+
+    ca = str(2)
+    cb = str(24.56)
+
+    print(aa)
+    print(ab)
+
+    print(ba)
+    print(bb)
+
+    print(ca)
+    print(cb)
+
+myStr = " This is a string which has a couple of words in it "
+
+print(myStr[1:5])
+print(myStr.strip())
+print(len(myStr))
+print(myStr.lower())
+print(myStr.upper())
+
+print('Enter your favourite game..')
+
+favGame = input()
+
+print(favGame + ' is a great game :)')
 
 """
 
-aa = int(2.99)
-ab = int("204")
+myList = ['cat', 'dog', 'elephant']
 
-ba = float(10)
-bb = float("122")
+print(myList)
+#print(myList[0])
 
-ca = str(2)
-cb = str(24.56)
+myList.append('frog')
+myList.pop(0)
 
-print(aa)
-print(ab)
+for item in myList:
+    print(item)
 
-print(ba)
-print(bb)
+myTuple = ('mario', 'luigi', 'peach')
 
-print(ca)
-print(cb)
+if "luigi" in myTuple:
+    print('loogoo here')
+
+mySet = {'facebook', 'google', 'reddit'}
+
+print('facebook' in mySet)
+
+mySet.add('twitter')
+
+mySet.update(['yahoo', 'youtube', 'vimeo'])
+
+print(len(mySet))
