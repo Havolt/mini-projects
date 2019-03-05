64a051c16f109316fa95fa4648c900ef0948f488
Mark Fitzpatrick <markfitz815@gmail.com>
2019-03-05 17:43:58 +0000

    sql: create valuesDifferent function and pass values to it

--- sqlDisplay/dev/public/scripts/signUpScr.js
+++ sqlDisplay/dev/public/scripts/signUpScr.js
@@ -1,5 +1,7 @@
 
 function checkUserInput() {
+    let passSame = false;
+    let emailSame = false;
     const userData = {
         username : document.querySelector('#cre-user__username').value,
         email : document.querySelector('#cre-user__email').value,
@@ -7,13 +9,50 @@ function checkUserInput() {
         email2 : document.querySelector('#cre-user__email-2').value,
         password2 : document.querySelector('#cre-user__password-2').value
     }
-    console.log(userData);
+    
+    if(userData.email === userData.email2) {
+        emailSame = true;
+    }
+    if(userData.password === userData.password2) {
+        passSame = true;
+    }
+
+    if(passSame && emailSame) {
+        sendUserInput(userData);
+    }else {
+        if(passSame) {
+            valuesDifferent({
+                el: document.querySelector('#cre-user__password'), 
+                warn: 'Passwords do not match!'
+            });
+        }
+        else if(emailSame) {
+            valuesDifferent({
+                el: document.querySelector('#cre-user__email'),
+                warn: 'Emails do not match!'
+            });
+        }
+        else {
+            valuesDifferent({
+                el: document.querySelector('#cre-user__password'), 
+                warn: 'Passwords do not match!'
+            },
+            {
+                el: document.querySelector('#cre-user__email'),
+                warn: 'Emails do not match!'
+            });
+        }
+    }
+    
+}
+
+function valuesDifferent(ob1, ob2) {
+    console.log(arguments);
 }
 
 
-function sendUserInput() {
-    
-    
+function sendUserInput(data) {
+    console.log(data)
 }
 
 function evtListners() {
