62d4129f8014f162f28aba37afb03b885d746de7
Mark Fitzpatrick <markfitz815@gmail.com>
2019-03-13 15:16:20 +0000

    sql: add styling to header and put username in header from cookies

--- sqlDisplay/dev/public/scripts/mainScr.js
+++ sqlDisplay/dev/public/scripts/mainScr.js
@@ -1,4 +1,14 @@
 
+function headerLogin(cookie) {
+    console.log(cookie);
+    const findSid = /(?:name=)+[^,;]*/g;
+    const cookieUsername = document.cookie.match(findSid)[0].split('').splice(5).join('');
+    console.log(cookieUsername);
+    
+    document.querySelector('.main__head__user__name').innerHTML=cookieUsername;
+}
+
+
 (function initMain() {
-    console.log('main function');
+    headerLogin(document.cookie);
 })();
