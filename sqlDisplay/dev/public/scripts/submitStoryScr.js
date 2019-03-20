fc622eb9c1412f5c64f8241e7cb406688406dd73
Mark Fitzpatrick <markfitz815@gmail.com>
2019-03-20 14:36:15 +0000

    sql: check if title or body of story is too short and send to function if one is

--- sqlDisplay/dev/public/scripts/submitStoryScr.js
+++ sqlDisplay/dev/public/scripts/submitStoryScr.js
@@ -12,8 +12,44 @@ function addSubmitEvtLstnrs() {
 
 function checkStory(title, body, genre) {
 
-    console.log(title, body, genre)
+    const failCheck = {
+        title: true,
+        body: true
+    }
+
+    if(title.length < 3) {
+        failCheck.title = true;
+    }
+    else {
+        title.split('').forEach(el => {
+            if(el != ' ') {
+                failCheck.title = false;
+            }
+        });
+    }
+    if(body.length < 50) {
+        failCheck.body = true;
+    }
+    else {
+        for(let i = 0; i < body.split('').length; i++) {
+            if(body.split('')[i] != ' ' && 
+            body.split('')[i] != '\r' && 
+            body.split('')[i] != '\n' ) {
+                failCheck.body = false;
+                break;
+            }
+        }
+    }
+
+    if(failCheck.title || failCheck.body) {
+        storyShortWarn(failCheck.title, failCheck.body);
+    }
 }
+
+function storyShortWarn(titleShort, bodyShort) {
+    console.log(titleShort, 'title too short');
+    console.log(bodyShort, 'body too short');
+};
 
 (function initSubmit(){
     addSubmitEvtLstnrs();
