84f27c625873aea0e4e63f2a3f69a16b764764ee
Mark Fitzpatrick <markfitz815@gmail.com>
2019-03-27 12:29:48 +0000

    sql: all data now displayed in stories page

--- sqlDisplay/dev/public/scripts/userStoryScr.js
+++ sqlDisplay/dev/public/scripts/userStoryScr.js
@@ -3,8 +3,15 @@ function setUpLayout() {
     const layoutData = JSON.parse(document.querySelector('.story_info').innerHTML)[0];
     document.querySelector('.story_info').remove();
 
-    
-    console.log(layoutData);
+    let newStoryDate = new Date(layoutData.date_created);
+
+    document.querySelector('.main__body__story-sec__title').innerHTML = layoutData.title;
+    document.querySelector('.main__body__story-sec__desc').innerHTML = layoutData.description;
+    document.querySelector('.main__body__story-sec__body').innerHTML = layoutData.body;
+    document.querySelector('.main__body__story-sec__misc__author').innerHTML = layoutData.author;
+    document.querySelector('.main__body__story-sec__misc__date').innerHTML = newStoryDate.toUTCString();
+
+    console.log(newStoryDate);
 }
 
 
