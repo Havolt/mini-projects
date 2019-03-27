15a27f898c93a7b3c8ada825bb4e71845c1eba2c
Mark Fitzpatrick <markfitz815@gmail.com>
2019-03-27 13:50:35 +0000

    sql: style stories page

--- sqlDisplay/dev/public/scripts/userStoryScr.js
+++ sqlDisplay/dev/public/scripts/userStoryScr.js
@@ -8,8 +8,8 @@ function setUpLayout() {
     document.querySelector('.main__body__story-sec__title').innerHTML = layoutData.title;
     document.querySelector('.main__body__story-sec__desc').innerHTML = layoutData.description;
     document.querySelector('.main__body__story-sec__body').innerHTML = layoutData.body;
-    document.querySelector('.main__body__story-sec__misc__author').innerHTML = layoutData.author;
-    document.querySelector('.main__body__story-sec__misc__date').innerHTML = newStoryDate.toUTCString();
+    document.querySelector('.main__body__story-sec__misc__author__link').innerHTML =  layoutData.author + ', ';
+    document.querySelector('.main__body__story-sec__misc__date').innerHTML = newStoryDate.toDateString();
 
     console.log(newStoryDate);
 }
